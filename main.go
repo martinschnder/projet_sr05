9a4833abb22fdb72e941d1a3ed2d645853f4a697
Martin <martin.schneider2001@gmail.com>
2023-04-24 17:32:29 +0200

    cleaned the display and added the shared clock

--- main.go
+++ main.go
@@ -1,23 +1,35 @@
 package main
 
 import (
-	"fmt"
-	"sync"
-	"time"
-	"strings"
 	"flag"
+	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
+	"sync"
+	"time"
 )
+
+var cyan string = "\033[1;36m"
+var raz string = "\033[0;00m"
+
+var h int = 0
+
+func max(x, y int) int {
+	if x < y {
+		return y
+	}
+	return x
+}
 
 func ecrivain(timer int) {
 	var nom = os.Getpid()
-	var h int = 0
-	
+
 	for {
 		mutex.Lock()
-        fmt.Printf(",=sender=%v,=hlg=%d\n", nom, h)
-        h = h+1
+		fmt.Printf(",=sender=%v,=hlg=%d\n", nom, h)
+    h++
 		mutex.Unlock()
 		time.Sleep(time.Duration(timer) * time.Second)
 	}
@@ -25,23 +37,31 @@ func ecrivain(timer int) {
 
 func lecteur() {
 	var rcvmsg string
-	l := log.New(os.Stderr, "", 0)
+	stderr := log.New(os.Stderr, "", 0)
 
-    for {
-        fmt.Scanln(&rcvmsg)
+	for {
+		fmt.Scanln(&rcvmsg)
 
 		mutex.Lock()
-        tab_allkeyval := strings.Split(rcvmsg[1:], rcvmsg[0:1])
+		tab_allkeyval := strings.Split(rcvmsg[1:], rcvmsg[0:1])
 
-        for _, keyval := range tab_allkeyval {
-            tab_keyval := strings.Split(keyval[1:], keyval[0:1])
-            l.Println(tab_keyval)
-            l.Println("  key : ", tab_keyval[0], " val : ", tab_keyval[1])
-        }
+    stderr.Printf("%s", cyan)
+		for _, keyval := range tab_allkeyval {
+			tab_keyval := strings.Split(keyval[1:], keyval[0:1])
+      key := tab_keyval[0]
+      value := tab_keyval[1]
+			// stderr.Println(tab_keyval)
+			stderr.Println("  key : ", key, " val : ", value)
+      if key == "hlg" {
+        h_recu, _ := strconv.ParseInt(value, 10, 16)
+        h = max(int(h_recu), h)
+      }
+		}
+    stderr.Printf("%s", raz)
 		mutex.Unlock()
 
 		rcvmsg = ""
-    }
+	}
 }
 
 var mutex = &sync.Mutex{}
