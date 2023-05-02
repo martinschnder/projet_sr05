cdeaace2942f1f6e28b6927162a56ff7f4ed15c5
Martin <martin.schneider2001@gmail.com>
2023-05-02 10:00:40 +0200

    changed request validation

--- net/net.go
+++ net/net.go
@@ -1,28 +1,28 @@
 package net
 
 import (
+	"container/list"
 	"fmt"
+	"os"
 	. "projet/message"
 	"projet/utils"
 	. "projet/utils"
-	"container/list"
-	"os"
 )
 
 const NB_SITES = 3
 
 type Net struct {
-	id       int
-	clock    int
-	tab      [NB_SITES]Request
-	messages chan MessageWrapper
-	server   *Server
-	color 	 string
-	initator bool
-	nbExpectedStates int
+	id                 int
+	clock              int
+	tab                [NB_SITES]Request
+	messages           chan MessageWrapper
+	server             *Server
+	color              string
+	initator           bool
+	nbExpectedStates   int
 	nbExpectedMessages int
-	state 	*State
-	globalStates *list.List
+	state              *State
+	globalStates       *list.List
 }
 
 func NewNet(id int, port string, addr string) *Net {
@@ -38,7 +38,7 @@ func NewNet(id int, port string, addr string) *Net {
 		}
 	}
 	n.server = NewServer(port, addr, id, n)
-  	utils.Info(n.id, "NewNet", "Successfully created server instance")
+	utils.Info(n.id, "NewNet", "Successfully created server instance")
 	n.messages = make(chan MessageWrapper)
 	n.color = "white"
 	n.initator = false
@@ -50,7 +50,7 @@ func NewNet(id int, port string, addr string) *Net {
 }
 
 func (n *Net) ReceiveCSrequest() {
-  utils.Info(n.id, "ReceiveCSRequest", "received cs request from server")
+	utils.Info(n.id, "ReceiveCSRequest", "received cs request from server")
 	n.clock += 1
 	n.tab[n.id] = Request{
 		RequestType: "access",
@@ -63,8 +63,8 @@ func (n *Net) ReceiveCSrequest() {
 		Content:     "",
 		Stamp:       n.clock,
 		MessageType: "LockRequestMessage",
-		VectClock: 	 n.state.VectClock,
-		Color: 		 n.color,
+		VectClock:   n.state.VectClock,
+		Color:       n.color,
 	}
 	n.writeMessage(msg)
 }
@@ -75,15 +75,15 @@ func (n *Net) receiveCSRelease() {
 		RequestType: "release",
 		Stamp:       n.clock,
 	}
-  utils.Info(n.id, "MessageHandler", "Server CS release received")
+	utils.Info(n.id, "MessageHandler", "Server CS release received")
 	msg := Message{
 		From:        n.id,
 		To:          -1,
 		Content:     "",
 		Stamp:       n.clock,
 		MessageType: "ReleaseMessage",
-		VectClock: 	 n.state.VectClock,
-		Color: 		 n.color,
+		VectClock:   n.state.VectClock,
+		Color:       n.color,
 	}
 	go n.writeMessage(msg)
 }
@@ -98,21 +98,21 @@ func (n *Net) receiveExternalMessage(msg Message) {
 		n.receiveReleaseMessage(msg)
 	case "AckMessage":
 		n.receiveAckMessage(msg)
- 	case "EditMessage":
-    n.server.SendMessage(msg.Content)
+	case "EditMessage":
+		n.server.SendMessage(msg.Content)
 	case "SnapshotMessage":
-    n.receiveSnapshotMessage(msg)
+		n.receiveSnapshotMessage(msg)
 	case "StateMessage":
-    n.receiveStateMessage(msg)
+		n.receiveStateMessage(msg)
 	case "PrepostMessage":
-    n.receivePrepostMessage(msg)
+		n.receivePrepostMessage(msg)
 	case "EndSnapshotMessage":
-    n.receiveEndSnapshotMessage()
+		n.receiveEndSnapshotMessage()
 	}
 }
 
 func (n *Net) receiveRequestMessage(received_msg Message) {
-  utils.Info(n.id, "receiveRequestMessage", "Received request message")
+	utils.Info(n.id, "receiveRequestMessage", "Received request message")
 	n.clock = Max(n.clock, received_msg.Stamp) + 1
 	n.tab[received_msg.From] = Request{
 		RequestType: "access",
@@ -124,52 +124,50 @@ func (n *Net) receiveRequestMessage(received_msg Message) {
 		Content:     "",
 		Stamp:       n.clock,
 		MessageType: "AckMessage",
-		VectClock: 	 n.state.VectClock,
-		Color: 		 n.color,
+		VectClock:   n.state.VectClock,
+		Color:       n.color,
 	}
-  utils.Info(n.id, "receiveRequestMessage", "Sending Ack message")
+	utils.Info(n.id, "receiveRequestMessage", "Sending Ack message")
 	go n.writeMessage(msg)
-	if n.tab[n.id].RequestType == "access" && n.isLastRequest() {
-    n.server.SendMessage("OkCs")
-    utils.Info(n.id, "MessageHandler", "Sending OkCs to server")
+	if n.tab[n.id].RequestType == "access" && n.isValidRequest() {
+		n.server.SendMessage("OkCs")
+		utils.Info(n.id, "MessageHandler", "Sending OkCs to server")
 	}
 }
 
-func (n *Net) isLastRequest() bool {
+func (n *Net) isValidRequest() bool {
 	for i := 0; i < NB_SITES; i++ {
 		if i != n.id {
-      if n.tab[i].RequestType == "Request" {
-        if n.tab[i].Stamp < n.tab[n.id].Stamp {
-              utils.Warning(n.id, "isLastRequest", "false")
+      if n.tab[i].Stamp < n.tab[n.id].Stamp {
+        utils.Warning(n.id, "isValidRequest", "false")
+        return false
+      } else if n.tab[i].Stamp == n.tab[n.id].Stamp {
+        if i < n.id {
+          utils.Warning(n.id, "isValidRequest", "false")
           return false
-        } else if n.tab[i].Stamp == n.tab[n.id].Stamp {
-          if i < n.id {
-                  utils.Warning(n.id, "isLastRequest", "false")
-            return false
-          }
         }
       }
 		}
 	}
-  utils.Warning(n.id, "isLastRequest", "true")
+	utils.Warning(n.id, "isValidRequest", "true")
 	return true
 }
 
 func (n *Net) receiveReleaseMessage(msg Message) {
-  utils.Info(n.id, "receiveReleaseMessage", "Received release message")
+	utils.Info(n.id, "receiveReleaseMessage", "Received release message")
 	n.clock = Max(n.clock, msg.Stamp) + 1
 	n.tab[msg.From] = Request{
 		RequestType: "release",
 		Stamp:       msg.Stamp,
 	}
-	if n.tab[n.id].RequestType == "access" && n.isLastRequest() {
-    n.server.SendMessage("OkCs")
-    utils.Info(n.id, "MessageHandler", "Sending OkCs to server")
+	if n.tab[n.id].RequestType == "access" && n.isValidRequest() {
+		n.server.SendMessage("OkCs")
+		utils.Info(n.id, "MessageHandler", "Sending OkCs to server")
 	}
 }
 
 func (n *Net) receiveAckMessage(msg Message) {
-  utils.Info(n.id, "MessageHandler", "Received ack message")
+	utils.Info(n.id, "MessageHandler", "Received ack message")
 	n.clock = Max(n.clock, msg.Stamp) + 1
 	if n.tab[msg.From].RequestType == "release" {
 		n.tab[msg.From] = Request{
@@ -177,17 +175,17 @@ func (n *Net) receiveAckMessage(msg Message) {
 			Stamp:       msg.Stamp,
 		}
 	}
-  utils.Warning(n.id, "receiveAckMessage", "request : " + n.tab[n.id].RequestType)
-	if n.tab[n.id].RequestType == "access" && n.isLastRequest() {
-    utils.Info(n.id, "MessageHandler", "Sending OkCs to server")
-    n.server.SendMessage("OkCs")
+	utils.Warning(n.id, "receiveAckMessage", "request : "+n.tab[n.id].RequestType)
+	if n.tab[n.id].RequestType == "access" && n.isValidRequest() {
+		utils.Info(n.id, "MessageHandler", "Sending OkCs to server")
+		n.server.SendMessage("OkCs")
 	}
 }
 
 func (n *Net) receiveSnapshotMessage(msg Message) {
 	utils.Error(n.id, "receiveSnapshotMessage", n.color)
 	utils.Info(n.id, "receiveSnapshotMessage", "Received snapshot message")
-	if (msg.Color == "red" && n.color == "white") {
+	if msg.Color == "red" && n.color == "white" {
 		utils.Error(n.id, "receiveSnapshotMessage", "SNAPSHOT MODE")
 
 		n.color = "red"
@@ -198,23 +196,23 @@ func (n *Net) receiveSnapshotMessage(msg Message) {
 			Content:     n.state.ToString(),
 			Stamp:       n.clock,
 			MessageType: "StateMessage",
-			VectClock: 	 n.state.VectClock,
-			Color: 		 n.color,
+			VectClock:   n.state.VectClock,
+			Color:       n.color,
 		}
 		utils.Info(n.id, "receiveSnapshotMessage", "Send state message")
 		go n.writeMessage(msg)
 	}
-	if (msg.Color == "white" && n.color == "red") {
+	if msg.Color == "white" && n.color == "red" {
 		utils.Info(n.id, "receiveSnapshotMessage", "Change to prepost message")
 		msg.MessageType = "PrepostMessage"
 		go n.writeMessage(msg)
 	}
-  }
+}
 
-  func (n *Net) receiveStateMessage(msg Message) {
+func (n *Net) receiveStateMessage(msg Message) {
 	utils.Info(n.id, "receiveStateMessage", "Received state message")
 	state := StateFromString(msg.Content)
-	if (n.initator) {
+	if n.initator {
 		n.globalStates.PushBack(state.ToString())
 		n.nbExpectedStates -= 1
 		n.nbExpectedMessages += state.Review
@@ -222,7 +220,7 @@ func (n *Net) receiveSnapshotMessage(msg Message) {
 		str := fmt.Sprintf("%d states and %d prepost messages to wait to finish the snapshot", n.nbExpectedStates, n.nbExpectedMessages)
 		utils.Info(n.id, "receiveStateMessage", str)
 
-		if (n.nbExpectedStates == 0 && n.nbExpectedMessages == 0) {
+		if n.nbExpectedStates == 0 && n.nbExpectedMessages == 0 {
 			utils.Warning(n.id, "receiveStateMessage", "Finish snapshot")
 
 			file, fileErr := os.Create("snapshot.txt")
@@ -241,11 +239,11 @@ func (n *Net) receiveSnapshotMessage(msg Message) {
 		utils.Info(n.id, "receiveStateMessage", "not initiator, msg resend")
 		go n.writeMessage(msg)
 	}
-  }
+}
 
-  func (n *Net) receivePrepostMessage(msg Message) {
+func (n *Net) receivePrepostMessage(msg Message) {
 	utils.Info(n.id, "receivePrepostMessage", "Received prepost message")
-	if (n.initator) {
+	if n.initator {
 		n.nbExpectedMessages -= 1
 
 		n.globalStates.PushBack(msg.ToString())
@@ -253,7 +251,7 @@ func (n *Net) receiveSnapshotMessage(msg Message) {
 		str := fmt.Sprintf("%d states and %d prepost messages to wait to finish the snapshot", n.nbExpectedStates, n.nbExpectedMessages)
 		utils.Info(n.id, "receivePrepostMessage", str)
 
-		if (n.nbExpectedStates == 0 && n.nbExpectedMessages == 0) {
+		if n.nbExpectedStates == 0 && n.nbExpectedMessages == 0 {
 			utils.Warning(n.id, "receivePrepostMessage", "Finish snapshot")
 
 			file, fileErr := os.Create("snapshot.txt")
@@ -272,37 +270,37 @@ func (n *Net) receiveSnapshotMessage(msg Message) {
 		utils.Info(n.id, "receivePrepostMessage", "not initiator, msg resend")
 		go n.writeMessage(msg)
 	}
-  }
+}
 
-  func (n *Net) reinitializeAfterSnapshot() {
+func (n *Net) reinitializeAfterSnapshot() {
 	n.initator = false
 	n.color = "white"
 	n.globalStates = list.New()
 	n.nbExpectedStates = 0
-	n.nbExpectedMessages = 0	
+	n.nbExpectedMessages = 0
 	msg := Message{
 		From:        n.id,
 		To:          -1,
 		Content:     "",
 		Stamp:       n.clock,
 		MessageType: "EndSnapshotMessage",
-		VectClock: 	 n.state.VectClock,
-		Color: 		 n.color,
+		VectClock:   n.state.VectClock,
+		Color:       n.color,
 	}
 	utils.Info(n.id, "reinitializeAfterSnapshot", "Send EndSnapshotMessage")
 	go n.writeMessage(msg)
-  }
+}
 
-  func (n *Net) receiveEndSnapshotMessage() {
+func (n *Net) receiveEndSnapshotMessage() {
 	n.color = "white"
 	n.globalStates = list.New()
-  }
+}
 
 func (n *Net) ReadMessage() {
 	var raw string
 	for {
 		fmt.Scanln(&raw)
-    	utils.Info(n.id, "ReadMessage", "Detected new message : " + raw)
+		utils.Info(n.id, "ReadMessage", "Detected new message : "+raw)
 		var msg = MessageFromString(raw)
 		if msg.From != n.id {
 			n.messages <- MessageWrapper{
@@ -327,7 +325,7 @@ func (n *Net) MessageHandler() {
 	for {
 		wrapperItem := <-n.messages
 		var msg = wrapperItem.Message
-    	utils.Info(n.id, "MessageHandler", "New message on the queue: " + msg.ToString())
+		utils.Info(n.id, "MessageHandler", "New message on the queue: "+msg.ToString())
 		if wrapperItem.Action == "send" {
 			utils.Info(n.id, "MessageHandler", "Spreading message on the ring")
 			if !msg.ConcernSnapshot() {
@@ -336,7 +334,7 @@ func (n *Net) MessageHandler() {
 			msg.Send()
 		} else if wrapperItem.Action == "process" {
 			if msg.To == n.id {
-        		utils.Info(n.id, "MessageHandler", "Handler processing the message")
+				utils.Info(n.id, "MessageHandler", "Handler processing the message")
 				n.state.Review -= 1
 				n.receiveExternalMessage(msg)
 			} else if msg.To == -1 && msg.From != n.id { // message for all
@@ -348,7 +346,7 @@ func (n *Net) MessageHandler() {
 				n.receiveExternalMessage(msg)
 			} else { // message not for us
 				// forward the message
-        		utils.Info(n.id, "MessageHandler", "Handler forwarding the message")
+				utils.Info(n.id, "MessageHandler", "Handler forwarding the message")
 				msg.Send()
 			}
 		}
@@ -369,7 +367,7 @@ func (n *Net) SendMessageFromServer(msg Message) {
 func (n *Net) InitSnapshot() {
 	n.color = "red"
 	n.initator = true
-	n.globalStates.PushBack(n.state.ToString()) 
+	n.globalStates.PushBack(n.state.ToString())
 	n.nbExpectedStates = NB_SITES - 1
 	n.nbExpectedMessages = n.state.Review
 
@@ -383,8 +381,8 @@ func (n *Net) InitSnapshot() {
 		Content:     "",
 		Stamp:       n.clock,
 		MessageType: "SnapshotMessage",
-		VectClock: 	 n.state.VectClock,
-		Color: 		 n.color,
+		VectClock:   n.state.VectClock,
+		Color:       n.color,
 	}
 
 	// data:= fmt.Sprintf("%+v", msg)
--- net/server.go
+++ net/server.go
@@ -1,133 +1,133 @@
 package net
 
 import (
+	"github.com/gorilla/websocket"
 	"net/http"
 	. "projet/message"
 	"projet/utils"
 	"sync"
-	"github.com/gorilla/websocket"
 )
 
 type Server struct {
-  socket *websocket.Conn
-  mutex *sync.Mutex
-  data []string
-  id int
-  net *Net
-  command Command
+	socket  *websocket.Conn
+	mutex   *sync.Mutex
+	data    []string
+	id      int
+	net     *Net
+	command Command
 }
 
 func NewServer(port string, addr string, id int, net *Net) *Server {
 	server := new(Server)
 	// s.clock = 0
-  server.socket = nil
-  server.net = net
-  server.mutex = &sync.Mutex{}
-  server.data = make([]string, 30)
-  server.data[0] = "Hello World!"
-  server.id = id
+	server.socket = nil
+	server.net = net
+	server.mutex = &sync.Mutex{}
+	server.data = make([]string, 30)
+	server.data[0] = "Hello World!"
+	server.id = id
 
 	go http.HandleFunc("/ws", server.createSocket)
 	go http.ListenAndServe(addr+":"+port, nil)
-  utils.Info(server.id, "newServer", "server listening on " + addr + ":" + port)
-  return server
+	utils.Info(server.id, "newServer", "server listening on "+addr+":"+port)
+	return server
 }
 
-func (server *Server)createSocket(w http.ResponseWriter, r *http.Request) {
-    var upgrader = websocket.Upgrader{
-        CheckOrigin : func(r *http.Request) bool { return true },
-    }
-    cnx,err := upgrader.Upgrade(w, r, nil)
-    if err != nil {
-        utils.Error(server.id, "ws_create", "Upgrade error : " + string(err.Error()))
-        return
-    }
-    utils.Info(server.id, "ws_create", "Client connecté")
-    server.socket = cnx
-    go server.receive()
-    server.Send()
+func (server *Server) createSocket(w http.ResponseWriter, r *http.Request) {
+	var upgrader = websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool { return true },
+	}
+	cnx, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		utils.Error(server.id, "ws_create", "Upgrade error : "+string(err.Error()))
+		return
+	}
+	utils.Info(server.id, "ws_create", "Client connecté")
+	server.socket = cnx
+	go server.receive()
+	server.Send()
 }
 
-func (server *Server)Send() {
-    if (server.socket == nil ) {
-         utils.Error(server.id, "ws_send", "websocket non ouverte")
-    } else {
-        err := server.socket.WriteJSON(server.data)
-        if err != nil {
-            utils.Error(server.id, "ws_send", "Error message : " + string(err.Error()))
-        } else {
-            utils.Info(server.id, "ws_send", "sending data to client")
-        }
-    }
+func (server *Server) Send() {
+	if server.socket == nil {
+		utils.Error(server.id, "ws_send", "websocket non ouverte")
+	} else {
+		err := server.socket.WriteJSON(server.data)
+		if err != nil {
+			utils.Error(server.id, "ws_send", "Error message : "+string(err.Error()))
+		} else {
+			utils.Info(server.id, "ws_send", "sending data to client")
+		}
+	}
 }
 
-func (server *Server)closeSocket() {
-    utils.Info(server.id, "ws_close", "Fin des réceptions => fermeture de la websocket")
-    server.socket.Close()
+func (server *Server) closeSocket() {
+	utils.Info(server.id, "ws_close", "Fin des réceptions => fermeture de la websocket")
+	server.socket.Close()
 }
 
-func (server *Server)receive() {
-    defer server.closeSocket()
-    for {
-        _, rcvmsg, err := server.socket.ReadMessage()
-        if err != nil {
-            utils.Error(server.id, "ws_receive", "ReadMessage : " + string(err.Error()))
-            break
-        }
-        
-        server.mutex.Lock()
-        command := ParseCommand(string(rcvmsg))
-        if (command.Action == "Snapshot") {
-          server.net.InitSnapshot()
-        } else {
-          server.command = command
-          server.net.ReceiveCSrequest()
-        }
-        utils.Info(server.id, "ws_receive", "réception : " + string(rcvmsg[:]) )
-        utils.Info(server.id, "ws_receive", "commande de type : " + string(command.Action) )
-        server.mutex.Unlock()
-    }
+func (server *Server) receive() {
+	defer server.closeSocket()
+	for {
+		_, rcvmsg, err := server.socket.ReadMessage()
+		if err != nil {
+			utils.Error(server.id, "ws_receive", "ReadMessage : "+string(err.Error()))
+			break
+		}
+
+		server.mutex.Lock()
+		command := ParseCommand(string(rcvmsg))
+		if command.Action == "Snapshot" {
+			server.net.InitSnapshot()
+		} else {
+			server.command = command
+			server.net.ReceiveCSrequest()
+		}
+		utils.Info(server.id, "ws_receive", "réception : "+string(rcvmsg[:]))
+		utils.Info(server.id, "ws_receive", "commande de type : "+string(command.Action))
+		server.mutex.Unlock()
+	}
 }
 
-func (server *Server)EditData(command Command) {
-  switch command.Action {
-  case "Remplacer":
-    server.data[command.Line - 1] = command.Content
-  case "Ajouter":
-    server.data[command.Line - 1] += command.Content
-  case "Supprimer":
-    server.data[command.Line - 1] = ""
-}
-  server.net.state.Text = server.data
+func (server *Server) EditData(command Command) {
+	switch command.Action {
+	case "Remplacer":
+		server.data[command.Line-1] = command.Content
+	case "Ajouter":
+		server.data[command.Line-1] += command.Content
+	case "Supprimer":
+		server.data[command.Line-1] = ""
+	}
+	server.net.state.Text = server.data
 
-  // server.forwardEdition(command)
-  // server.net.receiveCSRelease()
-  server.Send()
+	// server.forwardEdition(command)
+	// server.net.receiveCSRelease()
+	server.Send()
 }
 
-func (server *Server)forwardEdition(command Command) {
-  server.net.SendMessageFromServer(Message{
-    From: server.net.id,
-    To: -1,
-    Content: command.ToString(),
-    Stamp: server.net.clock,
-    MessageType: "EditMessage",
-    VectClock: 	 server.net.state.VectClock,
-    Color: "white",
-  })
+func (server *Server) forwardEdition(command Command) {
+	server.net.SendMessageFromServer(Message{
+		From:        server.net.id,
+		To:          -1,
+		Content:     command.ToString(),
+		Stamp:       server.net.clock,
+		MessageType: "EditMessage",
+		VectClock:   server.net.state.VectClock,
+		Color:       "white",
+	})
 }
 
 // Used by net to send a message to server
-func (server *Server)SendMessage(action string) {
-  if action == "OkCs" {
-    utils.Info(server.id, "ServSendMessage", "OkCs received" )
-    // Request accepted
-    server.EditData(server.command)
-    server.forwardEdition(server.command)
-    server.net.receiveCSRelease()
-  } else {
-    // Incoming command from another site
-    command := ParseCommand(action)
-    server.EditData(command)
-  }
+func (server *Server) SendMessage(action string) {
+	if action == "OkCs" {
+		utils.Info(server.id, "ServSendMessage", "OkCs received")
+		// Request accepted
+		server.EditData(server.command)
+		server.forwardEdition(server.command)
+		server.net.receiveCSRelease()
+	} else {
+		// Incoming command from another site
+		command := ParseCommand(action)
+		server.EditData(command)
+	}
 }
